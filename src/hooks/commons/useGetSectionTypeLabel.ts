a48ee46880054510d7eac6cd5e9bfbf39e5ed72b
EdsonNhancale <edsonnhancale97@gmail.com>
2025-01-31 16:12:35 +0200

    feat: added enum for sectionType

--- src/hooks/commons/useGetSectionTypeLabel.ts
+++ src/hooks/commons/useGetSectionTypeLabel.ts
@@ -1,9 +1,12 @@
-
 import { useUrlParams } from "./useQueryParams";
 
+enum SectionTypeProps { 
+    Student = "student", 
+    Staff = "staff" 
+}
 const useGetSectionTypeLabel = () => {
-    const { urlParameters } = useUrlParams()
-    const sectionType = urlParameters().sectionType ?? 'student';
+    const { urlParameters } = useUrlParams();
+    const sectionType: SectionTypeProps = urlParameters().sectionType as SectionTypeProps ?? SectionTypeProps.Student;
 
     return { sectionName: sectionType };
 }
