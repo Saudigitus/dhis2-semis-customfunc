a887782f69ad8abb3690d230fe406c8124b53a06
CelsoDeCarvalho <celsomomade361@gmail.com>
2025-02-21 11:51:21 +0200

    enhance(useDeleteTei.ts): add onComplete and onError optional params

--- src/hooks/tei/useDeleteTei.ts
+++ src/hooks/tei/useDeleteTei.ts
@@ -1,4 +1,5 @@
 import { useDataEngine } from "@dhis2/app-runtime";
+import { useState } from "react";
 
 const DELETE_TEI_MUTATION: any = {
     resource: 'trackedEntityInstances',
@@ -7,14 +8,27 @@ const DELETE_TEI_MUTATION: any = {
 }
 
 export function useDeleteTEI() {
-
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<unknown>(null)
     const engine = useDataEngine();
 
-    async function deleteTEI(trackedEntity: string) {
-
-        return await engine.mutate(DELETE_TEI_MUTATION, { variables: { id: trackedEntity } })
-            .catch((error) => console.log(error));
+    async function deleteTEI(trackedEntity: string, onComplete?: () => void, onError?: (error?: unknown) => void) {
+        try {
+            setLoading(true)
+            const response = await engine.mutate(DELETE_TEI_MUTATION, { variables: { id: trackedEntity } });
+            if (onComplete) {
+                onComplete()
+            }
+            return response;
+        } catch (error) {
+            setError(error)
+            if (onError) {
+                onError(error)
+            }
+        } finally {
+            setLoading(false)
+        }
     }
 
-    return { deleteTEI }
+    return { deleteTEI, loading, error }
 }
