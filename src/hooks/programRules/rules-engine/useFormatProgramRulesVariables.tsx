48822a4a1e937190b7c3f7a6e3cfc8c4486839b4
EdsonNhancale <edsonnhancale97@gmail.com>
2025-05-08 15:07:28 +0200

    added new program rule engine

---
+++ src/hooks/programRules/rules-engine/useFormatProgramRulesVariables.tsx
@@ -0,0 +1,12 @@
+import { useRecoilValue } from "recoil";
+import { ProgramRulesVariablesConfigState } from "src/schema/programRulesVariablesSchema";
+import { formatProgramRuleVariables } from "src/utils/programRules/formatProgramRules";
+
+
+export function useFormatProgramRulesVariables(program: string | null) {
+    const programRulesVariablesConfigState = useRecoilValue(ProgramRulesVariablesConfigState);
+
+    return {
+        programRulesVariables: formatProgramRuleVariables(programRulesVariablesConfigState, program),
+    }
+}
