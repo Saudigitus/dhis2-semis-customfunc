5e0e0291250d5aa3b9da8062610de756536e250e
CelsoDeCarvalho <celsomomade361@gmail.com>
2025-05-14 13:23:27 +0200

    Refactor validation logic and remove unused dependency

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,50 +3,50 @@ import React, { useState } from 'react'
 import { useValidation } from '../hooks/template_validation/useValidation'
 import { useValidateFile } from '../hooks/template_validation/useValidateFile'
 import program from "../utils/constants/programConfig.json"
-import { DropZone, ModalComponent } from 'dhis2-semis-components'
+// import { DropZone, ModalComponent } from 'dhis2-semis-components'
 
-const ImportProcess = () => {
-    const [open, setOpen] = useState(false)
-    const module = "final-result"
-    const [openPogress, setOpenProgress] = useState(false)
-    const UseValidation = new useValidation()
-    const { validador, invalidRecords, loader, validRecords } = useValidateFile(program, 'UPDATE')
+// const ImportProcess = () => {
+//     const [open, setOpen] = useState(false)
+//     const module = "final-result"
+//     const [openPogress, setOpenProgress] = useState(false)
+//     const UseValidation = new useValidation()
+//     const { validador, invalidRecords, loader, validRecords } = useValidateFile(program, 'UPDATE')
 
-    const onValidation = async (file: File) => {
-        UseValidation.setModule(module as unknown as any)
-        await UseValidation.validation(file[0])
-            .then((resp) => {
-                const { mapping, module } = resp
-                validador({ module, data: mapping })
-            })
-            .catch((error) => {
-                console.log(error, "ERROR")
-            })
-    }
+//     const onValidation = async (file: File) => {
+//         UseValidation.setModule(module as unknown as any)
+//         await UseValidation.validation(file[0])
+//             .then((resp) => {
+//                 const { mapping, module } = resp
+//                 validador({ module, data: mapping })
+//             })
+//             .catch((error) => {
+//                 console.log(error, "ERROR")
+//             })
+//     }
 
-    if(!loader){
-        console.log(invalidRecords, validRecords)
-    }
+//     if(!loader){
+//         console.log(invalidRecords, validRecords)
+//     }
 
-    return (
-        <>
-            <a style={{ width: "100%", cursor: "pointer", padding: "5px" }} onClick={(e) => {
-                e.preventDefault()
-                setOpen(true)
-            }}>
-                <button>Open mport</button>
-            </a>
+//     return (
+//         <>
+//             <a style={{ width: "100%", cursor: "pointer", padding: "5px" }} onClick={(e) => {
+//                 e.preventDefault()
+//                 setOpen(true)
+//             }}>
+//                 <button>Open mport</button>
+//             </a>
 
-            <ModalComponent
-                children={<DropZone accept='.csv,.xlsx' onSave={(file) => onValidation(file)} />}
-                handleClose={() => { setOpen(false) }}
-                loading={loader}
-                open={open}
-                title={"Bulk Data"}
-            />
-        </>
-    )
-}
+//             <ModalComponent
+//                 children={<DropZone accept='.csv,.xlsx' onSave={(file) => onValidation(file)} />}
+//                 handleClose={() => { setOpen(false) }}
+//                 loading={loader}
+//                 open={open}
+//                 title={"Bulk Data"}
+//             />
+//         </>
+//     )
+// }
 
 function MyApp() {
     // const { getData, tableData } = useTableData({ module: Modules.Transfer })
@@ -72,7 +72,8 @@ function MyApp() {
     // console.log(tableData)
     return (
         <div>
-            <ImportProcess/>
+            "WELLCOME"
+            {/* <ImportProcess/> */}
         </div>
     )
 
