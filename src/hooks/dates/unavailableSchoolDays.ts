e22a7624ba179d4184b7de0755691231b57baa92
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2025-08-30 10:55:44 +0200

    refactor: improve readability of unavailableDays and isClassPeriod functions

--- src/hooks/dates/unavailableSchoolDays.ts
+++ src/hooks/dates/unavailableSchoolDays.ts
@@ -14,16 +14,15 @@ export const unavailableSchoolDays = () => {
             return true
         }
 
-        if (sectionName == 'student')
-            if (isClassPeriod(date, config?.classPeriods) && isClassPeriod(date, [{ startDate: config?.academicYear?.startDate, endDate: config?.academicYear?.endDate }])) {
+        if (sectionName == 'student') {
+            if (isClassPeriod(new Date(date), config?.classPeriods) && isClassPeriod(new Date(date), [{ startDate: config?.academicYear?.startDate, endDate: config?.academicYear?.endDate }])) {
                 return false
             } else return true
+        }
 
         if (isClassPeriod(date, [{ startDate: config?.academicYear?.startDate, endDate: config?.academicYear?.endDate }])) {
             return false
         }
-
-
 
         return true
     }
@@ -43,11 +42,13 @@ export const unavailableSchoolDays = () => {
     }
 
     function isClassPeriod(date: Date, classPeriods: Array<{ startDate: string, endDate: string }>) {
-        return classPeriods.some(h => {
+        const response = classPeriods.some(h => {
             const start = new Date(h.startDate);
             const end = new Date(h.endDate);
             return start <= date && date <= end;
         })
+
+        return response
     }
 
     return {
