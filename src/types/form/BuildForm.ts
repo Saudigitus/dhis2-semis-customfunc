afb6ae5a4b601a98bd4966d5ac9243930618e0d1
cnandza <cnandza@saudigitus.org>
2025-09-16 14:01:21 +0200

    feat (): disabled school calendar options should not appear in academic year selectors on form

--- src/types/form/BuildForm.ts
+++ src/types/form/BuildForm.ts
@@ -1,10 +1,11 @@
-import { ProgramConfig, selectedDataStoreKey } from "dhis2-semis-types"
+import { ProgramConfig, SchoolCalendarDataStoreRecord, selectedDataStoreKey } from "dhis2-semis-types"
 import { Modules } from "dhis2-semis-types"
 
 type BuildFormType = {
     dataStoreData: selectedDataStoreKey
     programData: ProgramConfig
-    module: Modules
+    module: Modules, 
+    schoolCalendar?: SchoolCalendarDataStoreRecord
 }
 
 export { type BuildFormType }
