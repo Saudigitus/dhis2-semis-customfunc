af8399e71160569aaec0d7b70444b7a47ea66cb2
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2025-09-18 09:12:59 +0200

    feat(hooks): add keysToExclude param to filter query params
    Add optional keysToExclude parameter to useUrlParams hook to allow filtering out specific query parameters. This enables more flexible URL parameter management by excluding sensitive or unnecessary params from being included in the query string.

--- src/hooks/commons/useQueryParams.ts
+++ src/hooks/commons/useQueryParams.ts
@@ -1,37 +1,45 @@
-import { useSearchParams } from 'react-router-dom'
-import React from 'react'
+import { useSearchParams } from "react-router-dom";
+import React from "react";
 
-const useUrlParams = () => {
-    const [searchParams, setSearchParams] = useSearchParams()
+const useUrlParams = (keysToExclude?: string[]) => {
+    const [searchParams, setSearchParams] = useSearchParams();
 
-     const add = (key: string, value: string) => {
-        searchParams.set(key, value)
-        setSearchParams(searchParams)
-    }
+    const add = (key: string, value: string) => {
+        searchParams.set(key, value);
+        setSearchParams(searchParams);
+    };
 
     const remove = (key: string) => {
-        searchParams.delete(key)
-        setSearchParams(searchParams)
-    }
+        searchParams.delete(key);
+        setSearchParams(searchParams);
+    };
 
-    const query = React.useMemo(() => new URLSearchParams(searchParams), [searchParams])
+    const filteredQueryString = React.useMemo(() => {
+        const clone = new URLSearchParams(searchParams.toString());
+        keysToExclude?.forEach((key) => clone.delete(key));
+        return clone.toString();
+    }, [searchParams, keysToExclude]);
+
+    const query = React.useMemo(() => new URLSearchParams(searchParams), [searchParams]);
 
     const urlParameters = React.useMemo(
         () => ({
-            school: query.get('school'),
-            schoolName: query.get('schoolName'),
-            academicYear: query.get('academicYear'),
-            sectionType: query.get('sectionType'),
-            grade: query.get('grade'),
-            class: query.get('class'),
-            position: query.get('position'),
-            employmentType: query.get('employmentType'),
-            programStage: query.get('programStage'),
-            attendanceMode: query.get('attendanceMode'),
-            selectedDate: query.get('selectedDate')
-        }), [query]
-    )
+            school: query.get("school"),
+            schoolName: query.get("schoolName"),
+            academicYear: query.get("academicYear"),
+            sectionType: query.get("sectionType"),
+            grade: query.get("grade"),
+            class: query.get("class"),
+            position: query.get("position"),
+            employmentType: query.get("employmentType"),
+            programStage: query.get("programStage"),
+            attendanceMode: query.get("attendanceMode"),
+            selectedDate: query.get("selectedDate"),
+        }),
+        [filteredQueryString]
+    );
 
-    return { add, remove, useQuery: query, urlParameters }
-}
-export { useUrlParams }
+    return { add, remove, useQuery: query, urlParameters };
+};
+
+export { useUrlParams };
