76b10c9fbc5a053b0bb529fa41d943ceff1b9f27
cnandza <cnandza@saudigitus.org>
2026-07-06 15:34:05 +0200

    fix: add orgUnitMode ACCESSIBLE to getTeis call
    ensure API request fetches all tracked entity instances even when ownership isn't in the selected orgUnit

--- src/hooks/enrollmentDetails/useGetEnrollmentDetails.ts
+++ src/hooks/enrollmentDetails/useGetEnrollmentDetails.ts
@@ -16,7 +16,7 @@ export function useGetEnrollmentData(props: ExportData) {
         const trackedEntityIds = events?.map((x: { trackedEntity: string }) => x.trackedEntity).join(',')
 
         try {
-            return getTeis({ program: selectedSectionDataStore?.program as unknown as string, trackedEntities: trackedEntityIds, orgUnit })
+            return getTeis({ program: selectedSectionDataStore?.program as unknown as string, trackedEntities: trackedEntityIds, orgUnitMode: "ACCESSIBLE"})
                 .then(async (data: any) => {
                     let rows: any = []
                     let counter = 0
